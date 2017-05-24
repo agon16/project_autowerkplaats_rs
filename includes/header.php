06547e181831db4a8c114b11336ea1a6f5e00bfd
timmy1420 <tim.poco01@gmail.com>
2017-05-24 18:57:37 -0300

    PHP code weggehaald. Wordt vervangen met AJAX

--- includes/header.php
+++ includes/header.php
@@ -1,24 +1,13 @@
 <?php
 	if(isset($_SESSION['userID'])) {
 		$id = $_SESSION['userID'];
-
-		$sql = "SELECT busy FROM users WHERE id = '$id'";
-		$query = $conn->query($sql);
-
-		$result = $query->fetch_assoc();
-		$busy = $result['busy'];
-		if($busy == "yes") {
-			$busy = '<button onclick="state(\''.$id.'\');" style="margin-right: 20px" class="button special">Set status: <u>Beschikbaar</u></button>';
-		} else if($busy == "no") {
-			$busy = '<button onclick="state(\''.$id.'\');" style="margin-right: 20px" class="button">Set status: <u>Bezig</u></button>';
-		} 
 	}
 ?>
 <header id="header">
 	<a href="#" class="logo"><strong>RS Auto Werkplaats Web App</strong> developed by The Crew</a>
 
 	<ul class="icons">
-		<?php echo $busy; ?>
+		<button id="busy" onclick="state('<?php echo $id; ?>');" style="margin-right: 20px" class="button">Set status: <u>Bezig</u></button>
 		<li><a class="icon fa-bell-slash"><span class="label">Notifications</span></a></li>
 	</ul>
 </header>
