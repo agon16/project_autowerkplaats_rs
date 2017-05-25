fb2edfc79886cd04e8bd90b4bb313e164fb8d2c9
timmy1420 <tim.poco01@gmail.com>
2017-05-25 01:13:59 -0300

    Edit profile gefixed

--- edit_profile.php
+++ edit_profile.php
@@ -1,16 +1,11 @@
 <?php
 	session_start();
-
 	require 'includes/head.php';
 	require 'backend/db.php';
 
 	$box = "";
 
-	$id = $_SESSION['userID'];
-
-	if(!isset($_SESSION['userID'])) {
-		exit(); // Cancel loading page
-	}
+	$user_id = $_SESSION['userID'];
 
 	/**
 	* Add user
@@ -21,33 +16,45 @@
 		$address = $_POST['address'];
 		$email = $_POST['email'];
 		$phone = $_POST['phone'];
-		$image = $_POST['photo'];
-		$newImage = $target_dir.time().$_FILES["photo"]["name"]; //Renamed file
+		$target_dir = "uploads/users/";
+		$image = basename($_FILES["photo"]["name"]);
+		$ext = explode('.', $image);
+		$ext_count = count($ext); $ext_count = $ext_count-1;
+		$newImage = $target_dir.time().'.'.$ext[$ext_count]; //Renamed file
 		$password1 = sha1($_POST['password1']);
 		$password2 = sha1($_POST['password2']);
 
 		if($password1 == "da39a3ee5e6b4b0d3255bfef95601890afd80709" || $password2 == "da39a3ee5e6b4b0d3255bfef95601890afd80709") {
-			$sql = "UPDATE users SET firstname = '$firstname', lastname = '$lastname', address = '$address', email = '$email', phone = '$phone', image = '$newImage' WHERE id = '$id'";
+			$sql = "UPDATE users SET firstname = '$firstname', lastname = '$lastname', address = '$address', email = '$email', phone = '$phone'";
 		} else {
-			$sql = "UPDATE users SET firstname = '$firstname', lastname = '$lastname', address = '$address', email = '$email', phone = '$phone', image = '$newImage', password = '$password1' WHERE id = '$id'";
+			$sql = "UPDATE users SET firstname = '$firstname', lastname = '$lastname', address = '$address', email = '$email', phone = '$phone', password = '$password1'";
 		}
 
-		// Photo upload
-		$target_dir = "uploads/";
+		//Verify if an image has been selected
+		if(strlen($image) == 0) {
+			$sql_ = " WHERE id = '$user_id'";
+		} else {
+			$sql_ = ", image = '$newImage' WHERE id = '$user_id'";
+		}
+		
+		if($conn->query($sql.$sql_)) {
+			header("Location: view_profile.php");
+		} else {
+			$box = '<div class="box"><p>Foutmelding komt hierin. <b>Check dit</b></p></div>';
+		}
+
 		$target_file = $target_dir . $image;
 		$uploadOk = 1;
 		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 		// Check if image file is a actual image or fake image
-		if(isset($_POST["submit"])) {
-		    $check = getimagesize($_FILES["photo"]["tmp_name"]);
-		    if($check !== false) {
-		        // echo "File is an image - " . $check["mime"] . ".";
-		        $uploadOk = 1;
-		    } else {
-		        // echo "File is not an image.";
-		        $uploadOk = 0;
-		    }
-		}
+		$check = getimagesize($_FILES["photo"]["tmp_name"]);
+	    if($check !== false) {
+	        // echo "File is an image - " . $check["mime"] . ".";
+	        $uploadOk = 1;
+	    } else {
+	        // echo "File is not an image.";
+	        $uploadOk = 0;
+	    }
 		// Check if file already exists
 		if (file_exists($target_file)) {
 		    // echo "Sorry, file already exists.";
@@ -60,19 +67,9 @@
 		} else {
 		    move_uploaded_file($_FILES["photo"]["tmp_name"], $newImage); //Rename file
 		}
-		
-		if($password1 != $password2) { // Password verification
-			$box = '<div class="box"><p>Wachtwoord velden niet hetzelfde</p></div>';
-		} else if($conn->query($sql)) {
-			
 
-			// header("Location: overview_users.php");
-		} else {
-			$box = '<div class="box"><p>Foutmelding komt hierin. <b>Check dit</b></p></div>';
-		}
-			/* End isset */
 	} else {
-		$sql = "SELECT * FROM users WHERE id = '$id'";
+		$sql = "SELECT * FROM users WHERE id = '$user_id'";
 		$query = $conn->query($sql);
 
 		while ($result = $query->fetch_assoc()) {
@@ -101,7 +98,7 @@
 			<!-- Content -->
 			<section>
 				<header class="main">
-					<div align="center"><h2>Profiel gegevens bewerken</h2></div>
+					<div align="center"><h2>Gebruiker toevoegen</h2></div>
 				</header>
 
 				<!-- Content -->
@@ -112,26 +109,26 @@
 					<div class="6u 12u$(medium)">
 
 						<!-- Login form -->
-						<form method="post" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF'].'?id='.$id; ?>">
+						<form method="post" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 							<div class="row uniform">
 								<div class="12u 12u$(xsmall)">
-									<input name="firstname" id="firstname" value="<?php echo $firstname; ?>" placeholder="Voornaam" type="text">
+									<input name="firstname" required="" id="firstname" value="<?php echo $firstname; ?>" placeholder="Voornaam" type="text">
 								</div>
 								<div class="12u 12u$(xsmall)">
-									<input name="lastname" id="lastname" value="<?php echo $lastname; ?>" placeholder="Naam" type="text">
+									<input name="lastname" required="" id="lastname" value="<?php echo $lastname; ?>" placeholder="Naam" type="text">
 								</div>
 								<div class="12u 12u$(xsmall)">
-									<input name="email" id="email" value="<?php echo $email; ?>" placeholder="Email" type="text">
+									<input name="email" required="" id="email" value="<?php echo $email; ?>" placeholder="Email" type="text">
 								</div>
 								<div class="12u 12u$(xsmall)">
-									<input name="address" id="address" value="<?php echo $address; ?>" placeholder="Adres" type="text">
+									<input name="address" required="" id="address" value="<?php echo $address; ?>" placeholder="Adres" type="text">
 								</div>
 								<div class="12u 12u$(xsmall)">
-									<input name="phone" id="phone" value="<?php echo $phone; ?>" placeholder="Tel. nummer" type="text">
+									<input name="phone" required="" id="phone" value="<?php echo $phone; ?>" placeholder="Tel. nummer" type="text">
 								</div>
 								<div class="12u$">
 									<div class="select-wrapper">
-										<select name="user_role" id="user_role">
+										<select name="user_role" required="" id="user_role">
 											<option value="">- Gebruiker rol -</option>
 											<?php
 												$sql = "SELECT id, role FROM user_roles";
@@ -154,9 +151,9 @@
 								<!-- Break -->
 								<div class="12u$">
 									<ul class="actions">
-										<li><input value="Bewerken" class="special" name="update" type="submit"></li>
+										<li><input value="Terug" class="button" type="button" onclick="history.go(-1);"></li>
 										<li><input id="btnUpload" class="button" type="button" value="Foto uploaden"></li>
-										<li><a class="button" onclick="history.go(-1);">Terug</a></li>
+										<li><input value="Bewerken" class="special" name="update" type="submit"></li>
 									</ul>
 								</div>
 								<input style="display: none;" name="photo" type="file">
@@ -171,6 +168,10 @@
 			</section>
 
 		</div>
+		<!-- Avatars -->
+		<?php
+			require 'includes/avatars.php';
+		?>
 	</div>
 
 	<!-- Sidebar -->
